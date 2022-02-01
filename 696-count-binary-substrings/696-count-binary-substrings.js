e7ca89c11dd638f0125ee3f09c9f773d6432520a
Allen Gonzales <86206813+geearen@users.noreply.github.com>
2022-01-31 17:18:59 -0800

    Time: 156 ms (16.80%), Space: 45.3 MB (30.14%) - LeetHub

--- 696-count-binary-substrings/696-count-binary-substrings.js
+++ 696-count-binary-substrings/696-count-binary-substrings.js
@@ -3,21 +3,43 @@
  * @return {number}
  */
 var countBinarySubstrings = function(s) {
+    //O(N) solution
     
-    if(s.length <= 1) return 0;
-    let count = 0;
-    let prev = 0;
-    let curr = 1;
+//     if(s.length <= 1) return 0;
+//     let count = 0;
+//     let prev = 0;
+//     let curr = 1;
     
-    for(let i = 1; i < s.length; i++){
-      if(s[i] === s[i-1]){
-          curr++;
-      }else{
-          prev = curr;
-          curr = 1;
-      };
-      if(prev >= curr) count++;  
+//     for(let i = 1; i < s.length; i++){
+//       if(s[i] === s[i-1]){
+//           curr++;
+//       }else{
+//           prev = curr;
+//           curr = 1;
+//       };
+//       if(prev >= curr) count++;  
         
-    };
+//     };
+//     return count;
+    
+    let count = 0;
+    
+    for(let i = 0; i <s.length-1; i++){
+        
+        let leftPoint = s[i];
+        let rightPoint = s[i+1];
+        
+        if(leftPoint !== rightPoint){
+            let left = i;
+            let right = i+1;
+            
+            while(s[left] === leftPoint && s[right] === rightPoint){
+                count++;
+                left--;
+                right++;
+                
+            }
+        }
+    }
     return count;
 };
